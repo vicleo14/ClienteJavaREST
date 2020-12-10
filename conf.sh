7868e028964be7d0bcc4ab2df06cde3079acaaaa
Victor <vicleo.morales@hotmail.com>
2020-12-09 21:03:32 -0600

    Cambio a permisos sudo

--- conf.sh
+++ conf.sh
@@ -7,8 +7,11 @@ sudo apt install openjdk-8-jdk-headless
 sudo apt install unzip
 sudo apt install mysql-server
 
-sudo ./tomcat.sh
-sudo ./initDB.sh
+chmod +x tomcat.sh
+chmod +x initDB.sh
+chmod +x war.sh
+./tomcat.sh
+./initDB.sh
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
@@ -18,9 +21,9 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export CATALINA_HOME="/home/ubuntu/ClienteJavaREST/apache-tomcat-8.5.60"
 #export CATALINA_HOME="/mnt/E4C22EA7C22E7DC4/NOVENO_SEMESTRE/SistemasDistribuidos/Tarea8_MoralesFlores/apache-tomcat-8.5.60"
 sh $CATALINA_HOME/bin/catalina.sh start
-sudo ./war.sh
-sudo cp usuario_sin_foto.png apache-tomcat-8.5.60/webapps/ROOT/
-sudo cp WSClient.js apache-tomcat-8.5.60/webapps/ROOT/
-sudo cp prueba.html apache-tomcat-8.5.60/webapps/ROOT/
+./war.sh
+cp usuario_sin_foto.png apache-tomcat-8.5.60/webapps/ROOT/
+cp WSClient.js apache-tomcat-8.5.60/webapps/ROOT/
+cp prueba.html apache-tomcat-8.5.60/webapps/ROOT/
 
 
